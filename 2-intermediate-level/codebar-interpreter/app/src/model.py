8c545304396abab34682097fdf18203cc34b2ada
ErickBraganca <braganca.developer@gmail.com>
2023-01-03 18:26:07 -0300

    feat: implemented getters and stores

--- 2-intermediate-level/codebar-interpreter/app/src/model.py
+++ 2-intermediate-level/codebar-interpreter/app/src/model.py
@@ -1,29 +1,48 @@
 # Define a class of structured barcode functions.
-class Barcode_Rules():
-    #Return the region based in the range code
-    def get_region(data):
-        if data >= 1 and data <= 99:
-            return "Sudeste" 
-        elif data >= 100 and data <= 199:
+class barcode_rules():
+    # Return the region based in the range code
+    def get_region(region):
+        if (1 >= region <= 99):
+            return "Sudeste"
+        elif (100 >= region <= 199):
             return "Sul"
-        elif data >= 201 and data <= 299:
+        elif (201 >= region <= 299):
             return "Centro-Oeste"
-        elif data >= 300 and data <= 399:
+        elif (300 >= region <= 399):
+            return "Nordeste"
+        elif (400 >= region <= 499):
             return "Nordeste"
         else:
             return {'error': 101}
+    # -----------------------------------------#
+    # Vendor Getter
 
-    #Return product type based in the code
-    def get_product_type(data):
-        if data == '001':
-            return "Joias"
-        elif data == '111':
-            return "Livros"
-        elif data == '333':
-            return "Eletrônicos"
-        elif data == '555':
-            return "Bebidas"
-        elif data == '888':
-            return "Brinquedos"
-        else:
-            return {'error': 102}
+    def get_vendor(vendor):
+        vendor_store = {
+            '001': 'Joias',
+            '111': 'Livros',
+            '333': 'Eletrônicos',
+            '555': 'Bebidas',
+            '888': 'Brinquedos',
+            'ERR': 'Not Registered'
+        }
+        try:
+            return vendor_store[vendor]
+        except KeyError:
+            return vendor_store['ERR']
+    # -------------------------------------------#
+    # Product Type Getter
+
+    def get_product(product):
+        product_store = {
+            '001': 'Joias',
+            '111': 'Livros',
+            '333': 'Eletrônicos',
+            '555': 'Bebidas',
+            '888': 'Brinquedos',
+            'ERR': 'Not Registered'
+        }
+        try:
+            return product_store[product]
+        except KeyError:
+            return product_store['ERR']
