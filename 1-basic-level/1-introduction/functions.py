5111acbeadc31779bddbc54161e1db1e95744f78
Erick Bragança <70552360+ErickBraganca@users.noreply.github.com>
2023-01-31 07:35:21 -0300

    docs: update comments

--- 1-basic-level/1-introduction/functions.py
+++ 1-basic-level/1-introduction/functions.py
@@ -1,7 +1,9 @@
-# Função 1
-# Escreva uma função que recebe do usuário
-# um valor por parâmetro  e printa no console
-# o tipo desse valor recebido.
+'''
+Função 1:
+Escreva uma função que recebe do usuário
+um valor por parâmetro  e printa no console
+o tipo desse valor recebido.
+'''
 
 def check_inputed_type(value):
     value_type = type(value)
@@ -11,10 +13,12 @@ def check_inputed_type(value):
 inputed_value = input('Insira alguma coisa:')
 check_inputed_type(inputed_value)
 
-# Função 2
-# Escreva um array com números sequenciais
-# e uma função que retorna o elemento do array
-# com index 3.
+'''
+    Função 2:
+    Escreva um array com números sequenciais
+    e uma função que retorna o elemento do array
+    com index 3.
+'''
 
 def index_return():
     sequence = [1, 2, 3, 4, 5, 6]
@@ -22,15 +26,17 @@ def index_return():
 
 index_return()
 
-# Função 3
-# Escreva um algoritmo que possua um
-# array shackle= [150, 250, 500, 750, 1000]
-# e uma função que recebe do usuário um
-# valor de capacidade e que esse valor
-# seja removido do array shackle.
-# Retorne o novo array com o item escolhido
-# removido e caso o valor escolhido pelo usuário
-# não esteja no array informe ao usuário!
+'''
+    Função 3:
+    Escreva um algoritmo que possua um
+    array shackle= [150, 250, 500, 750, 1000]
+    e uma função que recebe do usuário um
+    valor de capacidade e que esse valor
+    seja removido do array shackle.
+    Retorne o novo array com o item escolhido
+    removido e caso o valor escolhido pelo usuário
+    não esteja no array informe ao usuário!
+'''
 
 def remove_item():
     shackle = [150, 250, 500, 750, 1000]
@@ -43,19 +49,22 @@ def remove_item():
 
 remove_item()
 
-# Escreva um algoritmo que possui dois
-# arrays, um chamado amount e outro expenses.
-# Solicite ao usuário a inserção de valores,
-# podendo ser eles positivos ou negativos de forma
-# que os valores positivos representem entradas e os
-# valores negativos despesas. Exemplo:
+'''
+    Função 4:
+    Escreva um algoritmo que possui dois
+    arrays, um chamado amount e outro expenses.
+    Solicite ao usuário a inserção de valores,
+    podendo ser eles positivos ou negativos de forma
+    que os valores positivos representem entradas e os
+    valores negativos despesas. Exemplo:
 
-#-35 #Despesas 
-#1000 ou +1000 #Receitas
+    -35 #Despesas 
+    1000 ou +1000 #Receitas
 
-#Depois escreva uma função que faça com que valores
-# positivos seja inseridos no array amount e valores
-# negativos sejam inseridos no array expenses.
+    Depois escreva uma função que faça com que valores
+    positivos seja inseridos no array amount e valores
+    negativos sejam inseridos no array expenses.
+'''
 
 amount = []
 expenses = []
@@ -75,3 +84,18 @@ def wallet():
     wallet()
 
 wallet()
+
+'''
+    Função 5:
+    Uma faculdade com nota mínima de 70, armazena em uma lista linear as notas de uma turma de 30 alunos.
+    tests_results = [100,70,65,85,89,95,99,30,20,10,35,67,55,59,54,78,84,97,60,50,49,25,80,75,96,100,74,80,60,78]
+
+    Base:
+    1 - Escreva um algorítimo que retorne a média geral da classe.
+    2 - Retorne em novos arrays as notas que estão abaixo e acima do valor
+    mínimo da faculdade assim como o numero de alunos em cada uma dessas listas.
+
+    Desafio:
+    3 - Retorne um novo array com as notas mínimas necessárias para que a média
+    entre esse novos valores e o array de notas abaixo de 70 alcansem o esperado para passar.
+'''
