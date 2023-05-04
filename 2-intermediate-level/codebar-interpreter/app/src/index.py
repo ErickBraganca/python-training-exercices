d74266d14fd68b5a024475ad14de2a97fc919ec0
ErickBraganca <braganca.designer@gmail.com>
2023-05-04 08:25:49 -0300

    feat: implemented a file write function

--- 2-intermediate-level/codebar-interpreter/app/src/index.py
+++ 2-intermediate-level/codebar-interpreter/app/src/index.py
@@ -1,62 +1,67 @@
-# Importing modules
-import model
-rule_model = model.barcode_model
+# Importing Package Module
+from model import Package
+import json
+import os
 
 # Input and output variables
 invalid_codes = []
 pack_list = []
 
+
 # Interpreter inicializator
-def init_interpreter(raw_codes):
+def init_interpreter(code_list):
     print("#-----Start Interpreter-----#")
 
     # Code validation to check right code nature
-    def code_validator(raw_codes):
-        for raw_code in raw_codes:
+    def code_validator(code_list):
+        for raw_code in code_list:
             valid_enumeration = raw_code.isdecimal()
             valid_quantity = len(raw_code)
 
-            if (valid_quantity == 15 and valid_enumeration):
-                print('Code Validated')
-                code_slicer(raw_code)
+            if valid_quantity == 15 and valid_enumeration:
+                print("Code Validated")
+                package_composer(raw_code)
             else:
                 invalid_codes.append(raw_code)
-                print('Code Invalid')
+                print("Code Invalid")
+
     # ----------------------------------------#
-    # Code splitation in specific sentence
-    def code_slicer(valid_code):
-        # Dummy Code Splited
-        splited_code = {
-            "origem":       valid_code[0:3],
-            "destino":      valid_code[3:6],
-            "identificador": valid_code[6:9],
-            "vendedor":     valid_code[9:12],
-            "produto":      valid_code[12:16],
-        }
-        package_composer(splited_code)
-    # ----------------------------------------#
-    # Package composer
-    def package_composer(splited_code):
+    # Code splitation and package composer
+    def package_composer(valid_code):
+        # Code splitation
+        origin_code = valid_code[0:3]
+        destination_code = valid_code[3:6]
+        company_code = valid_code[6:9]
+        vendor_code = valid_code[9:12]
+        product_code = valid_code[12:16]
         # Dummy of package object
-        pack_object = {
-            'origem':       '',
-            'destino':      '',
-            'identificador': '',
-            'vendedor':     '',
-            'produto':      '',
-        }
+        package_instance = Package(
+            origin_code, destination_code, company_code, vendor_code, product_code
+        )
+        package_object = package_instance.get_package()
 
-        pack_object['origem'] = rule_model.get_region(splited_code['origem'])
-        pack_object['destino'] = rule_model.get_region(splited_code['destino'])
-        pack_object['vendedor'] = rule_model.get_vendor(splited_code['vendedor'])
-        pack_object['produto'] = rule_model.get_product(splited_code['produto'])
-        pack_object['identificador'] = splited_code['identificador']
+        write_data(package_object)
+        # pack_list.append(package_object)
 
-        pack_list.append(pack_object)
     # ----------------------------------------#
     # Call the enterpoint in interpreter inicializator
-    code_validator(raw_codes)
+    code_validator(code_list)
     print("#-----End Interpreter-----#")
 
     return pack_list
+
+
 # ----------------------------------------#
+def write_data(package):
+    FILE_FOLDER = "data"
+    FILE_NAME = "db.txt"
+    JSON_SEPARATOR = (',', ':')
+
+    FILE_PATH = os.path.join(FILE_FOLDER, FILE_NAME)
+    print(FILE_PATH)
+
+    with open(FILE_PATH, "a", encoding="utf-8") as content:
+        json.dump(package, content, indent=4, separators=JSON_SEPARATOR)
+
+
+init_interpreter(['288355555123888'])
