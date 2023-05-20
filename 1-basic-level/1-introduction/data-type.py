2693d5ba0e69acea602d6a110414f0031b8006d4
ErickBraganca <braganca.designer@gmail.com>
2023-05-20 13:36:08 -0300

    feat: add more types

--- 1-basic-level/1-introduction/data-type.py
+++ 1-basic-level/1-introduction/data-type.py
@@ -1,22 +1,17 @@
 # Variables
-# A memory space used to store data
-# Sintaxe: < variable name > = < data type >
-
-name = "Erick Bragança"
-user = 'José Carlos'
-age = 47
-is_registered = True
+# Sintaxe: < Variable Name > = < Data Type >
 
 # Main Python Data Types
 # Integers
 # Float
-# Strings
 # Boolean
+# Strings
 # Lists
 # Tuples
 # Dictionary
 # Sets
-# -----------Numbers------------
+
+# ---------Numbers------------
 # Integer (int)
 negative_integer = -1, -2, -3, -4
 positive_integer = 1, 2, 3, 4, 5
@@ -25,20 +20,24 @@ positive_integer = 1, 2, 3, 4, 5
 negative_floating = -0.25, -23.5
 positive_floating = 0.67, 1.256
 
-# -----------Strings (Text)------------
+# Complex (complex)
+complex_number = 2 + 3j
+
+# -------Booleans (bol)------
+is_conected = True
+is_loged = False
+
+# -------Sequences------------
+# Strings (str)
 strings = 'Developer'
 setence = "My name is Erick"
 
-# -------Booleans (True or False)------
-isConected = True
-isLoged = False
-
-# -------lists (Arrays)-----------------
+# lists (list)
 weight_list = ['350kg', '30kg', '156kg']
 expenses = [1234, 4567, 7896]
 bidimentional = [[122, 345],[678, 890]]
 
-# -------Dictionaries (Objects)----------
+# Dictionaries (dict)
 user = {
     'name': 'Erick',
     'age': 40,
@@ -46,3 +45,9 @@ user = {
     'isRegistered': True,
     'codes': [123, 345, 567]
 }
+
+# Tuples (tuple)
+first_numbers = (1, 2, 3)
+
+# Sets (set)
+data_ints = {1, 2, 3}
